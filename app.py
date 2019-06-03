0ae59c6d59067093c68f2ab7f299f9d8ac9be7c1
Miguel Grinberg <miguel.grinberg@gmail.com>
2019-06-03 16:40:37 +0100

    updated all the requirements to current versions

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 import time
 from flask import Flask, request, render_template, session, flash, redirect, \
     url_for, jsonify
-from flask.ext.mail import Mail, Message
+from flask_mail import Mail, Message
 from celery import Celery
 
 
